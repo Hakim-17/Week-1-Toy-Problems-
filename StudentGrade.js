622cb6362d0a621c3b9f4b7b18df0599fd25e89f
Abdulhakim <abdulhakim.abdalla@student.moringaschool.com>
2023-12-17 14:35:12 +0300

    Student Grades

---
+++ StudentGrade.js
@@ -0,0 +1,20 @@
+function studentsGrade(grade){
+
+    if (grade > 79){
+        return 'A';
+    }
+    else if (grade >= 60 && grade < 80){
+        return 'B';
+    }
+    else if (grade >= 49 && grade < 60){
+        return 'C';
+    }
+    else if (grade >= 40 && grade < 49){
+        return 'D';
+    }
+    else if (grade < 40){
+        return 'E';
+    }
+}
+
+console.log(studentsGrade(39));
